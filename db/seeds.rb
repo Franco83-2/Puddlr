73c7d2a112844b4579e9da54c2f0579afe3b74c2
franco83-2 <franco.84@gmail.com>
2017-02-06 10:39:44 -0500

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,40 +20,36 @@
 # Puddle.create(driver_id: Driver.second.id, seats: 4, departure_time: (DateTime.now - 5))
 # PuddlePassenger.create(passenger_id: Passenger.first.id, puddle_id: Puddle.first.id)
 # PuddlePassenger.create(passenger_id: Passenger.second.id, puddle_id: Puddle.first.id)
-10.times do
+20.times do
   Company.create(name: Faker::Company.name)
 end
 
-10.times do
+20.times do
   User.create(password: "password", password_confirmation: "password", email: Faker::Internet.email, company_id: rand(1..10))
 end
 
-10.times do |i|
+20.times do |i|
   Profile.create(name: Faker::Name.name, address: "#{Faker::Address.street_address}, #{Faker::Address.city}", phone: "#{Faker::PhoneNumber.subscriber_number(10)}", user_id: i+1)
 end
 
-10.times do |i|
+20.times do |i|
   Location.create(address: "#{Faker::Address.street_address}", name: "#{Faker::Address.city}", company_id: i+1)
   Location.create(address: "#{Faker::Address.street_address}", name: "#{Faker::Address.city}", company_id: i+1)
   Location.create(address: "#{Faker::Address.street_address}", name: "#{Faker::Address.city}", company_id: i+1)
 end
 
-10.times do |i|
+20.times do |i|
   Driver.create(user_id: i+1)
 end
 
-10.times do |i|
+20.times do |i|
   Passenger.create(user_id: i+1)
 end
 
-20.times do |i|
-  Puddle.create(driver_id: rand(1..10), seats: 10, departure_time: (DateTime.now + rand(2..5)))
+100.times do |i|
+  Puddle.create(driver_id: rand(1..10), seats: 5, departure_time: (DateTime.now + rand(2..5)))
 end
 
-20.times do |i|
-  Puddle.create(driver_id: rand(1..10), seats: 10, departure_time: (DateTime.now - rand(2..10)))
-end
-
-40.times do |i|
-  PuddlePassenger.create(passenger_id: rand(1..10), puddle_id: (1..20))
+100.times do |i|
+  PuddlePassenger.create(passenger_id: rand(1..10), puddle_id: rand(1..100))
 end
